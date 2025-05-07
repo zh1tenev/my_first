973d78f5987bd78ae2dc094618196ccfbd1481c1
Oleg <oleg@openres.ru>
2025-05-07 15:36:05 +0700

    Arithmetic operations

---
+++ src/main/java/org/first/calc/Calc.java
@@ -0,0 +1,16 @@
+package org.first.calc;
+
+public class Calc {
+    public static void main(String[] args) {
+        int two = 2;
+        int six = 6;
+        int four = 4;
+        int five = 5;
+        int sixDivTwo = six / two;
+        int fiveMinusTwo = five - two;
+        int fourTimeTwo = four * two;
+        System.out.println(sixDivTwo);
+        System.out.println(fiveMinusTwo);
+        System.out.println(fourTimeTwo);
+    }
+}
