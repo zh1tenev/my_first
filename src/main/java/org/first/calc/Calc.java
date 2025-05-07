3bf77685e93444f6b8f5ab228bb0f34c5a2abb67
Oleg <oleg@openres.ru>
2025-05-07 15:41:18 +0700

    Arithmetic operations

--- src/main/java/org/first/calc/Calc.java
+++ src/main/java/org/first/calc/Calc.java
@@ -8,9 +8,9 @@ public class Calc {
         int five = 5;
         int sixDivTwo = six / two;
         int fiveMinusTwo = five - two;
-        int fourTimeTwo = four * two;
+        int fourMultiplyTwo = four * two;
         System.out.println(sixDivTwo);
         System.out.println(fiveMinusTwo);
-        System.out.println(fourTimeTwo);
+        System.out.println(fourMultiplyTwo);
     }
 }
