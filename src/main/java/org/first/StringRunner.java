45eafff0a816a850e791ef6fcdf970aa3a9d2bf9
zh1tenev <zhitenevoo@gmail.com>
2025-05-07 17:59:03 +0700

    String + string short concatenation

---
+++ src/main/java/org/first/StringRunner.java
@@ -0,0 +1,12 @@
+package org.first;
+
+public class StringRunner {
+    public static void main(String[] args) {
+        String idea = "Hello World!";
+        System.out.println(idea);
+        idea += " But I am a newbie. ";
+        int year = 2025;
+        idea += year;
+        System.out.println(idea);
+    }
+}
