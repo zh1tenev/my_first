20af3027a71d930197aca5ec1d65dead3087fce0
zh1tenev <zhitenevoo@gmail.com>
2025-05-08 08:56:26 +0700

    manual tests

--- src/main/java/org/first/calc/Conv.java
+++ src/main/java/org/first/calc/Conv.java
@@ -12,5 +12,14 @@ public class Conv {
     public static void main(String[] args) {
         System.out.println("Одна тысяча монет в долларах это: " + Conv.rubToDollar(1000));
         System.out.println("Одна тысяча монет в евро это: " + Conv.rubToEuro(1000));
+        float input = 183.4F;
+        float expected = 2;
+        float output = rubToEuro(input);
+        boolean passed = expected == output;
+        input = 161.54F;
+        float output2 = rubToDollar(input);
+        boolean passed2 = expected == output2;
+        System.out.println("183.4 coins int Euro are 2. Test result : " + passed);
+        System.out.println("161.14 coins are 2. Test result : " + passed2);
     }
 }
