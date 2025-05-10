f6b584a7f207596d8e8c25486ff654fb7a8f4a43
zh1tenev <zhitenevoo@gmail.com>
2025-05-10 20:28:21 +0700

    if-else structure

--- src/main/java/org/first/AlertDivByZero.java
+++ src/main/java/org/first/AlertDivByZero.java
@@ -4,12 +4,13 @@ public class AlertDivByZero {
     public static void checkNumber(int number) {
         if (number < 0) {
             System.out.println(number + ": this is negative number.");
+        } else {
+            System.out.println(number + ": this is positive number.");
         }
     }
 
     public static void main(String[] args) {
         AlertDivByZero.checkNumber(4);
-        AlertDivByZero.checkNumber(0);
         AlertDivByZero.checkNumber(-4);
     }
 }
