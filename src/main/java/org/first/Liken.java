e935a97735ec8ae3bc8e20359c8cd1fbc84efa8c
zh1tenev <zhitenevoo@gmail.com>
2025-05-10 20:17:35 +0700

    compare operators

---
+++ src/main/java/org/first/Liken.java
@@ -0,0 +1,18 @@
+package org.first;
+
+public class Liken {
+
+    public static void main(String[] args) {
+        int first = 10;
+        int second = 9;
+
+        boolean result = first > second;
+                System.out.println(result);
+
+        result = first < second;
+                System.out.println(result);
+
+        result = first == second;
+                System.out.println(result);
+    }
+}
