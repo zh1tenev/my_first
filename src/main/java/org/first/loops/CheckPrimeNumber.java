99ef640235d544fd2e0cd259ef8261678564c303
zh1tenev <zhitenevoo@gmail.com>
2025-05-15 22:40:03 +0700

    count prime number

--- src/main/java/org/first/loops/CheckPrimeNumber.java
+++ src/main/java/org/first/loops/CheckPrimeNumber.java
@@ -11,4 +11,14 @@ public class CheckPrimeNumber {
         }
         return prime;
     }
+
+    public static int calc(int finish) {
+        int count = 0;
+        for (int number = 2; number <= finish; number++) {
+            if (CheckPrimeNumber.check(number)) {
+                count++;
+            }
+        }
+        return count;
+    }
 }
--- src/test/java/org/first/loops/CheckPrimeNumberTest.java
+++ src/test/java/org/first/loops/CheckPrimeNumberTest.java
@@ -53,4 +53,44 @@ class CheckPrimeNumberTest {
         boolean result = CheckPrimeNumber.check(number);
         assertThat(result).isFalse();
     }
+
+    @Test
+    void when5Then3() {
+        int finish = 5;
+        int result = CheckPrimeNumber.calc(finish);
+        int expected = 3;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void when11Then5() {
+        int finish = 11;
+        int result = CheckPrimeNumber.calc(finish);
+        int expected = 5;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void when2Then1() {
+        int finish = 2;
+        int result = CheckPrimeNumber.calc(finish);
+        int expected = 1;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void when25Then9() {
+        int finish = 25;
+        int result = CheckPrimeNumber.calc(finish);
+        int expected = 9;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void when49Then15() {
+        int finish = 49;
+        int result = CheckPrimeNumber.calc(finish);
+        int expected = 15;
+        assertThat(result).isEqualTo(expected);
+    }
 }
