bd09c7dfbad1129ccd6493629c1db4c2169e154b
zh1tenev <zhitenevoo@gmail.com>
2025-05-16 22:31:26 +0700

    draw x

---
+++ src/main/java/org/first/loops/Slash.java
@@ -0,0 +1,26 @@
+package org.first.loops;
+
+public class Slash {
+    public static void draw(int size) {
+        for (int row = 0; row < size; row++) {
+            for (int cell = 0; cell < size; cell++) {
+                if (row == cell) {
+                    System.out.print("0");
+                } else if (size == cell + row + 1) {
+                    System.out.print("0");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Draw by 3");
+        draw(3);
+        System.out.println("Draw by 5");
+        draw(5);
+    }
+}
