2d17f3ad7f37a7782c01bd1550de6f9a96c815bc
zh1tenev <zhitenevoo@gmail.com>
2025-05-17 08:27:54 +0700

    array and loop

---
+++ src/main/java/org/first/array/ArrayLoop.java
@@ -0,0 +1,13 @@
+package org.first.array;
+
+public class ArrayLoop {
+    public static void main(String[] args) {
+        int[] a = new int[5];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = i * 2 + 3;
+        }
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
