0889431ce5430bd55b4e9838df78ab88ff5a4493
zh1tenev <zhitenevoo@gmail.com>
2025-05-17 08:44:44 +0700

    add tests

---
+++ src/test/java/org/first/array/SquareTest.java
@@ -0,0 +1,23 @@
+package org.first.array;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class SquareTest {
+    @Test
+    void whenBound3Then014() {
+        int bound = 3;
+        int[] result = Square.calculate(bound);
+        int[] expected = new int[] {0, 1, 4};
+        assertThat(result).containsExactly(expected);
+    }
+
+    @Test
+    void whenBound5Then014916() {
+        int bound = 5;
+        int[] result = Square.calculate(bound);
+        int[] expected = new int[] {0, 1, 4, 9, 16};
+        assertThat(result).containsExactly(expected);
+    }
+}
