9ec5248f54bcdc367a59dfb1a96714585fa73dbc
zh1tenev <zhitenevoo@gmail.com>
2025-05-17 08:23:19 +0700

    add in array and then take

--- src/main/java/org/first/array/ArrayDefinition.java
+++ src/main/java/org/first/array/ArrayDefinition.java
@@ -8,5 +8,15 @@ public class ArrayDefinition {
         System.out.println("Размер массива равен: " + a.length);
         System.out.println("Размер массива равен: " + b.length);
         System.out.println("Размер массива равен: " + c.length);
+        b[0] = "First";
+        b[1] = "Second";
+        b[2] = "Third";
+        b[3] = "Fourth";
+        b[4] = "Fifth";
+        System.out.println(b[0]);
+        System.out.println(b[1]);
+        System.out.println(b[2]);
+        System.out.println(b[3]);
+        System.out.println(b[4]);
     }
 }
