4b3ab72e9931478bf174296edaa45a91ef60ea2c
zh1tenev <zhitenevoo@gmail.com>
2025-05-17 08:15:07 +0700

    array length

--- src/main/java/org/first/array/ArrayDefinition.java
+++ src/main/java/org/first/array/ArrayDefinition.java
@@ -5,5 +5,8 @@ public class ArrayDefinition {
         short[] a = new short[10];
         String[] b = new String[100500];
         float[] c = new float[40];
+        System.out.println("Размер массива равен: " + a.length);
+        System.out.println("Размер массива равен: " + b.length);
+        System.out.println("Размер массива равен: " + c.length);
     }
 }
