1434a9691ffbcddc262f1d625aa690c42871f870
zh1tenev <zhitenevoo@gmail.com>
2025-05-17 09:02:59 +0700

    swap data in array witch param

--- src/test/java/org/first/array/SwitchArrayTest.java
+++ src/test/java/org/first/array/SwitchArrayTest.java
@@ -36,4 +36,24 @@ class SwitchArrayTest {
         int[] expected = {1};
         assertThat(result).containsExactly(expected);
     }
+
+    @Test
+    void whenSwap0to3() {
+        int[] input = {1, 2, 3, 4};
+        int source = 0;
+        int destination = input.length - 1;
+        int[] result = SwitchArray.swap(input, source, destination);
+        int[] expected = {4, 2, 3, 1};
+        assertThat(result).containsExactly(expected);
+    }
+
+    @Test
+    void whenSwap4to5() {
+        int[] input = {1, 2, 3, 4, 5, 6};
+        int source = 3;
+        int destination = 4;
+        int[] result = SwitchArray.swap(input, source, destination);
+        int[] expected = {1, 2, 3, 5, 4, 6};
+        assertThat(result).containsExactly(expected);
+    }
 }
