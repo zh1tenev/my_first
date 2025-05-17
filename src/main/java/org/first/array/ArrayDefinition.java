23113691c9c9d88b342248db9a685ec349cf26bb
zh1tenev <zhitenevoo@gmail.com>
2025-05-17 08:12:55 +0700

    init array

---
+++ src/main/java/org/first/array/ArrayDefinition.java
@@ -0,0 +1,9 @@
+package org.first.array;
+
+public class ArrayDefinition {
+    public static void main(String[] args) {
+        short[] a = new short[10];
+        String[] b = new String[100500];
+        float[] c = new float[40];
+    }
+}
