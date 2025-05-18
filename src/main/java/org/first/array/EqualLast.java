b63bcc8986118adada2992ed091e91d913eb8f42
zh1tenev <zhitenevoo@gmail.com>
2025-05-18 22:38:17 +0700

    check last item in arrays

---
+++ src/main/java/org/first/array/EqualLast.java
@@ -0,0 +1,7 @@
+package org.first.array;
+
+public class EqualLast {
+    public static boolean check(int[] left, int[] right) {
+        return left[left.length - 1] == right[right.length - 1];
+    }
+}
