36a62364c0995de2f541ee1cd92252945332102d
zh1tenev <zhitenevoo@gmail.com>
2025-05-18 22:34:44 +0700

    check prefix

---
+++ src/main/java/org/first/array/ArrayChar.java
@@ -0,0 +1,14 @@
+package org.first.array;
+
+public class ArrayChar {
+    public static boolean startsWith(char[] word, char[] prefix) {
+        boolean result = true;
+        for (int i = 0; i < prefix.length; i++) {
+            if (word[i] != prefix[i]) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}
---
+++ src/test/java/org/first/array/ArrayCharTest.java
@@ -0,0 +1,23 @@
+package org.first.array;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class ArrayCharTest {
+    @Test
+    public void whenStartWithPrefixThenTrue() {
+        char[] word = {'H', 'e', 'l', 'l', 'o'};
+        char[] prefix = {'H', 'e'};
+        boolean result = ArrayChar.startsWith(word, prefix);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    public void whenNotStartWithPrefixThenFalse() {
+        char[] word = {'H', 'e', 'l', 'l', 'o'};
+        char[] prefix = {'H', 'i'};
+        boolean result = ArrayChar.startsWith(word, prefix);
+        assertThat(result).isFalse();
+    }
+}
