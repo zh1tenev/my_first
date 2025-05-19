2bfd9e86fa2798484e8bd3903c9a7fb7f0a0b10b
zh1tenev <zhitenevoo@gmail.com>
2025-05-19 21:33:48 +0700

    min in array's diapason

--- src/main/java/org/first/array/Min.java
+++ src/main/java/org/first/array/Min.java
@@ -10,4 +10,14 @@ public class Min {
         }
         return min;
     }
+
+    public static int findMin(int[] array, int start, int finish) {
+        int min = array[start];
+        for (int index = start; index <= finish; index++) {
+            if (array[index] < min) {
+                min = array[index];
+            }
+        }
+        return min;
+    }
 }
