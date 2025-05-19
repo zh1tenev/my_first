28ce575095e2e6a8030a30cc9b4a6b1d213262a4
zh1tenev <zhitenevoo@gmail.com>
2025-05-19 21:26:55 +0700

    min in array

---
+++ src/main/java/org/first/array/Min.java
@@ -0,0 +1,13 @@
+package org.first.array;
+
+public class Min {
+    public static int findMin(int[] array) {
+        int min = array[0];
+        for (int index = 1; index < array.length; index++) {
+            if (array[index] < min) {
+                min = array[index];
+            }
+        }
+        return min;
+    }
+}
